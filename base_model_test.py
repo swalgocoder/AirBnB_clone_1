c516b0f7108f3221ac4432072eabb9960646d37d
gary gao <gary.d.gao@gmail.com>
2017-02-11 08:09:38 -0800

    unittest

---
+++ base_model_test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import unittest
+from  models import BaseModel
+import datetime
+
+
+class BaseModelTest(unittest.TestCase):
+    def  setUp(self):
+        """
+        objects to be tested
+        """
+        self.model1_test = BaseModel()
+        self.model2_test = BaseModel()
+
+    def basic_setup_test(self):
+        """
+        to_json method of BaseModel class
+        """
+        self.assertTrue(hasattr(self.model1_test, "id"))
+        self.assertTrue(hasattr(self.model1_test, "__class__"))
+        self.assertTrue(hasattr(self.model1_test, "created_at"))
+        self.assertTrue(hasattr(self.model1_test, "updated_at"))
+        self.assertTrue(self.model1_test.id != self.model2_test.id)
+        model1_ctime = self.model1_test.created_at
+        model2_ctime = self.model2_test.created_at
+        self.assertTrue(model1_ctime != modle2_ctime)
+        self.assertTrue(type(model1_ctime) is datetime.datetime)
+
+    def test_save(self):
+        """
+        saving updates the updated_at attribute
+        """
+        model1_utime = self.model1_test.updated_at
+        self.model1_test.save()
+        modle1_stime = self.model1_test.updated_at
+        self.assertFalse(model1_utime == model1_stime)
+
+    def test_to_json(self):
+        """
+        to_json method testing
+        """
+        model2_testid = self.model2_test.id
+        jsondict_obj = self.model2_test.to_json()
+        self.assertNotEqual(self.model2_test.__dict__, jsondict_obj)
+        self.assertEqual(jsondict_obj["id"], self.model2_test.__dict__["id"])
+        self.assertNotEqual(jsondict_obj["created_at"],
+                            self.model2_test.__dict__["created_at"])
+        self.assertNotEqual(type(jsondict_obj["created_at"]),
+                            type(self.model2_test.__dict__["created_at"]))
+
+if __name__ == '__main__':
+    unittest.main()
