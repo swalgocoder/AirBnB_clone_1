8c006f766fc0099b9f501d0a324d997caae7060d
gary gao <gary.d.gao@gmail.com>
2017-02-10 17:15:24 -0800

    unittesting

--- my_unittests.py
+++ my_unittests.py
@@ -13,7 +13,7 @@ class TestingBaseModel(unittest.TestCase):
         self.model1_test = BaseModel()
         self.model2_test = BaseModel()
 
-    def test_setup_instantiation(self):
+    def setup_instantiation_test(self):
         """
         testing to_json method  of BaseModel class
          """
@@ -21,6 +21,9 @@ class TestingBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(self.model1_test, "__class__"))
         self.assertTrue(hasattr(self.model1_test, "created_at"))
         self.assertTrue(hasattr(self.model1_test, "updated_at"))
+        
+    def setup_reinstantiation_test
+        self.assertIsInstance(self.model2_test, BaseModel)
         self.assertTrue(self.model1_test.id != self.model2_test.id)
         m1_obj = self.model1_test.created_at
         m2_obj = self.model2_test.created_at
@@ -31,10 +34,12 @@ class TestingBaseModel(unittest.TestCase):
         """
         testing if save updates the updated_at attribute
         """
-        m1_obj_u = self.model1_test.updated_at
+        self.assertFalse(hasattr(self.model1_test, "updated_at"))
         self.model1_test.save()
-        m1_obj_s = self.model1_test.updated_at
-        self.assertFalse(m1_obj_u == m1_obj_s)
+        self.assertTrue(hasattr(self.model1_test, "updated_at"))
+        old_time_obj= self.model2_test.update_at
+        self.model2_test.save()
+        self.assertNotEqual(old_time_obj, self.model2.updated_at)
 
     def test_to_json(self):
         """
