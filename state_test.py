967509078d9d9019f32237d83eae4d2d48364d17
gary gao <gary.d.gao@gmail.com>
2017-02-11 22:37:25 -0800

    testting

--- state_test.py
+++ state_test.py
@@ -10,14 +10,13 @@ class StateTest(unittest.TestCase):
         objects for testing
         """
         self.model1_test = State()
-        self.model2_test = State()
+
 
     def test_instantiation(self):
         """
         class attributes initialization testing
          """
         self.assertIsInstance(self.model1_test, State)
-        self.assertIsInstance(self.model2_test, State)
         self.assertTrue(hasattr(self.model1_test, "name"))
 
     def test_types(self):
