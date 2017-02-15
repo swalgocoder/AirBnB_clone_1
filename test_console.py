13ec52e0f7c62b07706378aa3798154a83acb7c1
gary gao <gary.d.gao@gmail.com>
2017-02-15 11:56:02 -0800

    a

--- test_console.py
+++ test_console.py
@@ -3,12 +3,16 @@
 The TestConsole Module defines a TestConsole class that
 inherits from unittest.TestCase for testing Hosh class, an interactive
 shell based on cmd.Cmd
+credit of  "test_create_object"etc, for this module goes to Danton Rodriguez
+(https://github.com/p0516357)
 """
 import sys
 import unittest
 from unittest import mock
 from unittest.mock import patch
 from io import StringIO
+from test import support
+from test.support import captured_stdout, captured_stderr
 from console import Hosh
 
 """
@@ -18,6 +22,11 @@ class TestConsole(unittest.TestCase):
     """
     Create automated tests for interactive shell based on cmd module
     """
+    no_instance = "** no instance found **\n"
+    no_class = "** class doesn't exist **\n"
+    missing_class = "** class name missing **\n"
+    missing_id = "** instance id missing **\n"
+    
     def setUp(self):
         """setup method for Console Test Class"""
         self.mock_stdin = unittest.mock.create_autospec(sys.stdin)
@@ -46,20 +55,51 @@ class TestConsole(unittest.TestCase):
         cli = self.create()
         self.assertTrue(cli.onecmd("EOF"))
 
+    def test_create_object(self):
+        """test method for do_create method errors"""
+        cli = self.create()
+        with captured_stdout() as stdout, captured_stderr() as stderr:
+            expected = "** class name missing **\n"
+            self.assertFalse(cli.onecmd("create"))
+            self.assertEqual(expected, stdout.getvalue())
+
+        with captured_stdout() as stdout, captured_stderr() as stderr:
+            expected = "** class doesn't exist **\n"
+            self.assertFalse(cli.onecmd("create airplanes"))
+            self.assertEqual(expected, stdout.getvalue())
+        
+
+    def test_show_object(self):
+        """test method for do_show method errors"""
+        cli = self.create()
+        with captured_stdout() as stdout, captured_stderr() as stderr:
+            self.assertFalse(cli.onecmd("show"))
+            self.assertEqual(TestConsole.missing_class, stdout.getvalue())
+        with captured_stdout() as stdout, captured_stderr() as stderr:
+            self.assertFalse(cli.onecmd("show Korea"))
+            self.assertEqual(TestConsole.no_class, stdout.getvalue())
+        with captured_stdout() as stdout, captured_stderr() as stderr:
+            self.assertFalse(cli.onecmd("show BaseModel AA"))
+            self.assertEqual(TestConsole.no_instance, stdout.getvalue())
+        with captured_stdout() as stdout, captured_stderr() as stderr:
+            self.assertFalse(cli.onecmd("show BaseModel"))
+            self.assertEqual(TestConsole.missing_id, stdout.getvalue())
+        
+
     def test_destroy_object(self):
         """test method for do_destroy"""
         cli = self.create()
-        bad_input_str = 'destroy BaseModel This-is-testing-98'
+        bad_input = 'destroy BaseModel This-is-testing-98'
         with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            self.assertFalse(cli.onecmd(input_str))
+            self.assertFalse(cli.onecmd(bad_input))
         self.assertEqual('** no instance found **', '** no instance found **')
 
     def test_update(self):
         """test do_update"""
         cli = self.create()
-        bad_in_put_str = 'update BaseModel This-is-testing-98'
+        bad_input = 'update BaseModel This-is-testing-98'
         with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            self.assertFalse(cli.onecmd(in_put))
+            self.assertFalse(cli.onecmd(bad_input))
         self.assertEqual('** value missing **', '** value missing **')
 
     def test_all(self):
@@ -72,9 +112,6 @@ class TestConsole(unittest.TestCase):
             self.assertFalse(cli.onecmd('classname.all()'))
         self.assertEqual("** class doesn't exist **",
                          "** class doesn't exist **")
-"""
-if __name__ == '__main__':
-    unittest.main()
-"""
+
 suite = unittest.TestLoader().loadTestsFromTestCase(TestConsole)
 unittest.TextTestRunner(verbosity = 2).run(suite)
